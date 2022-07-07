3b3afbecaecbde59f18b9b08e9e33a84f767bc11
suyeee <suyeon0727@naver.com>
2022-07-07 17:38:01 +0900

    2012~2014, 전국의 실효습도 수집

--- sample_dataset/sample_data3.py
+++ sample_dataset/sample_data3.py
@@ -96,14 +96,6 @@ send_slack('크롤링 시작')
 
 driver = get_chrome_driver()
 
-# # 브라우저 창 한개 더 열기
-# driver.execute_script('window.open("about:blank", "_blank");')
-
-# # 브라우저 인덱스
-# tabs = driver.window_handles
-
-# # 첫번째 브라우저 -> 2013년 크롤링
-# driver.switch_to.window(tabs[0])
 driver.get('https://data.kma.go.kr/climate/ehum/selectEhumChart.do?pgmNo=110')
 
 # 지점선택
@@ -114,35 +106,6 @@ time.sleep(3)
 driver.find_element(By.XPATH, '//*[@id="ztree_1_check"]').click()
 time.sleep(2)
 
-# ASOS에 없는 지점들은 석택해제
-# 116(관악산) 제외
-driver.find_element(By.XPATH, '//*[@id="ztree_2_switch"]').click()
-driver.find_element(By.XPATH, '//*[@id="ztree_3_check"]').click()  # 선택해제
-time.sleep(2)
-
-# 176(대구(기))
-driver.find_element(By.XPATH, '//*[@id="ztree_7_switch"]').click()
-driver.find_element(By.XPATH, '//*[@id="ztree_9_check"]').click()  # 선택해제
-time.sleep(2)
-
-# 214(삼척)
-driver.find_element(By.XPATH, '//*[@id="ztree_26_switch"]').click()
-driver.find_element(By.XPATH, '//*[@id="ztree_32_check"]').click()  # 선택해제
-time.sleep(2)
-
-# 전라남도 - 164(무안), 256(주암), 175(진도(첨찰산)) 제외
-driver.find_element(By.XPATH, '//*[@id="ztree_66_switch"]').click()
-driver.find_element(By.XPATH, '//*[@id="ztree_71_check"]').click()  # 164 선택해제
-driver.find_element(By.XPATH, '//*[@id="ztree_78_check"]').click()  # 256 선택해제
-driver.find_element(By.XPATH, '//*[@id="ztree_79_check"]').click()  # 175 선택해제
-time.sleep(2)
-
-# 제주도 - 187(성산), 265(성산포) 제외
-driver.find_element(By.XPATH, '//*[@id="ztree_113_switch"]').click()
-driver.find_element(By.XPATH, '//*[@id="ztree_117_check"]').click()  # 187 선택해제
-driver.find_element(By.XPATH, '//*[@id="ztree_118_check"]').click()  # 265 선택해제
-time.sleep(2)
-
 # 선택완료 버튼 클릭
 driver.find_element(By.XPATH, '//*[@id="sidetreecontrol"]/a').click()
 time.sleep(2)
@@ -150,19 +113,24 @@ time.sleep(2)
 # 데이터 수집    
 datas = []
    
-for day in range(20120201,20120502):
+for day in range(20120101,20140620):
     
     if ((str(day)[-2:] < '32') & (str(day)[-2:] != '00')) & ((str(day)[4:6] < '13') & (str(day)[4:6] != '00')):
         try:
             next_date(driver,day)
-            crawling(driver)
-            time.sleep(3)
+            
+            # 시작일이 올바르지 않을경우
+            driver.find_element(By.CSS_SELECTOR, '.buttonOK').click()
+            time.sleep(2)
+            print('no crawling')
             print(day)
         except:
             try:
-                # 시작일이 올바르지 않을경우
-                driver.find_element(By.CSS_SELECTOR, '.buttonOK').click()
-                time.sleep(2)    
+                # 날짜가 정상적인 경우 데이터 수집
+                crawling(driver)
+                time.sleep(3)
+                print('yes')
+                print(day)        
             except:
                 print('error!')
     else:
